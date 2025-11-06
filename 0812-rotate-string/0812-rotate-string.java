76efb920e99a4cfe585767c7371bcd9a605eebb5
KumarPradeep <134432786+pradeepkumarputi@users.noreply.github.com>
2025-11-06 16:42:10 +0530

    Time: 0 ms (100%), Space: 42.4 MB (7.87%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,18 +1,12 @@
 class Solution {
-    public String rightRotate(String s, int k) {
-    int n = s.length();
-    k = k % n;
-
-    return s.substring(n - k) + s.substring(0, n - k);
-}
+    
     public boolean rotateString(String s, String goal) {
         if(s.length() != goal.length()){
             return false;
         }
-        for(int i=1;i<=s.length();i++){
-            if(rightRotate(s,i).equals(goal)){
-                return true;
-            }
+        String result=s+s;
+        if(result.contains(goal)){
+            return true;
         }
         return false;
     }
